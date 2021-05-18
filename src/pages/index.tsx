2fe3d6e732331a44e9dd5b12a3aa6e43ae28c5b5
Gabriel Kayque Campos <bielgkc@gmail.com>
2021-05-18 13:14:56 -0300

    challenge

---
+++ src/pages/index.tsx
@@ -0,0 +1,35 @@
+import { CompletedChallenges } from "../components/CompletedChallenges";
+import { CountDown } from "../components/CountDown";
+import { ExperienceBar } from "../components/ExperienceBar";
+import { Profile } from "../components/Profile";
+import { ChallengeBox } from "../components/ChallengeBox";
+import { CountdownProvider } from "../contexts/CountdownContext";
+
+import Head from "next/head";
+
+import styles from "../styles/pages/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Inicio | Move</title>
+      </Head>
+
+      <ExperienceBar />
+
+      <CountdownProvider>
+        <section>
+          <div>
+            <Profile />
+            <CompletedChallenges />
+            <CountDown />
+          </div>
+          <div>
+            <ChallengeBox />
+          </div>
+        </section>
+      </CountdownProvider>
+    </div>
+  );
+}
